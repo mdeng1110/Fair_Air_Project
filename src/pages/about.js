bea623aea04e77cd97be2c4c1bb82af1dfbd86ea
mchen <chen.mengyuan@gmail.com>
2021-07-30 21:43:52 -0700

    Minor changes for responsive pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './about.css'
+
 const About = () => {
   return (
     <div class = "about">
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,69 +1,39 @@
-import React, { useState } from "react";
-import "./contact.css";
-import { db } from "./firebase";
+import {FaGithub} from 'react-icons/fa';
+
+import React from 'react';
+import './contact.css'
 
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const [loader, setLoader] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    db.collection("contacts")
-      .add({
-        name: name,
-        email: email,
-        message: message,
-      })
-      .then(() => {
-        setLoader(false);
-        alert("Your message has been submitted👍");
-      })
-      .catch((error) => {
-        alert(error.message);
-        setLoader(false);
-      });
-
-    setName("");
-    setEmail("");
-    setMessage("");
-  };
-
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <h1>Contact Us 💌</h1>
-
-      <label>Name</label>
-      <input
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      <label>Email</label>
-      <input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      <label>Message</label>
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      ></textarea>
-
-      <button
-        type="submit"
-        style={{ background: loader ? "#ccc" : " #02A899" }}
-      >
-        Submit
-      </button>
-    </form>
+    <div class = "contact">
+      <div class="contact-section">
+        <div class="contact-inner-container">
+            <h1>Contact Us</h1>
+            <p class="text">
+            <p>💌 Email: fairairproject@gmail.com</p>
+            <p><FaGithub />  Github: https://github.com/mdeng1110/Fair_Air_Project</p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <p> </p>
+            </p>
+        </div>
+    </div>
+    </div>
   );
 };
 
