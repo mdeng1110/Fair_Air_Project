7d35f458ccd7e58b345db98fc63b6dbffaf33683
mdeng1110 <maydeng1110@yahoo.com>
2021-08-06 00:06:13 -0700

    updated build_app.sh

--- infrastructure/build_app.sh
+++ infrastructure/build_app.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+# download node.js package
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+
+# download NPM
+# binary for NPM
+curl https://nodejs.org/dist/v14.17.4/node-v14.17.4-linux-x64.tar.xz --output node-v14.tar.xz 
+
+# extract with tar
+
+# path to node.js
+
+# download AWS
+
+# extract AWS
+
+# update AWS CLI
